40e1949df0f784d34108e969dc8fa2ee2081d734
a-kurski <132788909+a-kurski@users.noreply.github.com>
2026-02-19 14:41:14 +0100

    feat: import basic plotter in main

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import numpy as np
+from basic_plot import plot_basic
 
+D = np.array(
+    [
+        [2, 0, 1],
+        [1.08, 1.68, 2.38],
+        [-0.83, 1.82, 2.49],
+        [-1.97, 0.28, 2.15],
+        [-1.31, -1.51, 2.59],
+        [0.57, -1.91, 4.32]
+    ]
+)
 
-def plot():
-    pass
+T = np.array([1, 2, 3, 4, 5, 6], dtype=float)
 
 def linear_estimation(independent, dependent, learning_rate, num_iterations):
     pass
@@ -10,7 +21,7 @@ def quadratic_estimation(independent, dependent, learning_rate, num_iterations):
     pass
 
 def main():
-    pass
+    plot_basic(D)
 
 if __name__ == '__main__':
     main()
