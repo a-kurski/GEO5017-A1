0256e027d54a9fdf38674938297ddb6de69eb4f7
a-kurski <132788909+a-kurski@users.noreply.github.com>
2026-02-19 14:31:44 +0100

    minor changes

---
+++ main.py
@@ -0,0 +1,16 @@
+
+
+def plot():
+    pass
+
+def linear_estimation(independent, dependent, learning_rate, num_iterations):
+    pass
+
+def quadratic_estimation(independent, dependent, learning_rate, num_iterations):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
